d1dbb435cbd4561df87a8d5f3e2db19fffb6bc6a
stangatimu <stangatimu@gmail.com>
2020-02-09 18:38:00 +0300

    added respondToInvitation logic

--- logic/invitation.js
+++ logic/invitation.js
@@ -10,7 +10,7 @@ const Invitation = {
             sender:Joi.string().min(3).max(30).required().pattern(/^[a-zA-Z ]*$/),
             recepient:Joi.object({
                 name: Joi.string().min(3).max(30).required().pattern(/^[a-zA-Z ]*$/),
-                alias:Joi.string().alphanum().min(3).max(10).pattern(/^[a-zA-Z0-9 ]*$/)
+                alias:Joi.string().min(3).max(10).pattern(/^[a-zA-Z0-9 ]*$/)
             }).required()
         });
 
@@ -36,6 +36,41 @@ const Invitation = {
 
         // findById and return
         return await mongoose.model('Invitation').findById(_id).lean();
+    },
+    async respondToInvitation(params,req){
+
+        // validate id
+        if(!params._id) throw new Error("invitation id is required")
+        if(!mongoose.isValidObjectId(params._id)) throw new Error("provided id is not a valid id");
+
+        // validate data object
+        const data_schema = Joi.object({
+            _id:Joi.string().alphanum().required(),
+            comment:Joi.string().max(150),
+            status:Joi.boolean()
+        });
+
+        // validate recieved data
+        const is_valid = data_schema.validate(params);
+
+        // check and throw joi validation error
+        if(is_valid.error){
+            throw new Error(is_valid.error.details[0].message);
+        }
+
+        // update invitation
+        const invitation = await mongoose.model('Invitation').findById(params._id);
+
+        if(!invitation) return false;
+
+        // update invitation
+        invitation.status = params.status;
+        invitation.comment = params.comment;
+
+        // save invitation
+        await invitation.save();
+
+        return true;
     }
 }
 module.exports = Invitation;
--- tests/unit/initation/respond_test.js
+++ tests/unit/initation/respond_test.js
@@ -6,9 +6,10 @@ const mongoose = require('mongoose');
 const { Invitation } = require("../../../logic");
 
 describe("Invitation", function(){
-    describe("Accept invitation", function(){
+    describe("Respond invitation", function(){
+        let data;
         this.beforeAll( async ()=>{
-            let data = await mongoose.model('Invitation').create({
+            data = await mongoose.model('Invitation').create({
                 recepient:{
                     name:"lupita kare",
                     alias:"Lulu"
@@ -21,9 +22,52 @@ describe("Invitation", function(){
         });
 
 
-        it("should require invitation id to be provided")
-        it("should require provided id to be a valid object id")
-        it("should should return false if invitation is not found")
-        it("should should update invitation status and return true")
+        it("should require invitation id to be provided", async ()=>{
+            let params = {
+                status:true,
+                comment:"Thanks for the invite"
+            }
+            try{
+                const result = await Invitation.respondToInvitation(params);
+                assert.isUndefined(result,"expect error got result");
+            }catch(err){
+                assert.equal(err.message,"invitation id is required");
+            }
+        })
+        it("should require provided id to be a valid object id", async ()=>{
+            let params = {
+                _id:"not a valid object id",
+                status:true,
+                comment:"Thanks for the invite"
+            }
+            try{
+                const result = await Invitation.respondToInvitation(params);
+                assert.isUndefined(result,"expect error got result");
+            }catch(err){
+                assert.equal(err.message,"provided id is not a valid id");
+            }
+        })
+        it("should should return false if invitation is not found", async ()=>{
+            let params = {
+                _id:String(new mongoose.Types.ObjectId()),
+                status:true,
+                comment:"Thanks for the invite"
+            }
+            const result = await Invitation.respondToInvitation(params);
+            assert.isFalse(result);
+        })
+        it("should should update invitation status and return true", async ()=>{
+            let params = {
+                _id:data.id,
+                status:true,
+                comment:"Thanks for the invite"
+            }
+            const result = await Invitation.respondToInvitation(params);
+            const invitation = await Invitation.getById(data.id);
+            assert.isTrue(result);
+            assert.isTrue(invitation.status);
+            assert.equal(invitation.comment,params.comment);
+
+        })
     })
 })
